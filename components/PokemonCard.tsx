4499f93702cdbcab8c180f0924d0ed3795e13c9a
Jose Roncal <joseluis.rome@protonmail.com>
2023-10-05 03:39:18 -0500

    feat: :sparkles: add shadow effect for pokemon cards
    Add shadow effect with conditional logic for pokemon cards

--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -12,6 +12,7 @@ interface PokemonCardProps {
 interface CardScheme {
   border: string;
   text: string;
+  shadow: string;
 }
 
 function PokemonCard(props: PokemonCardProps) {
@@ -19,27 +20,59 @@ function PokemonCard(props: PokemonCardProps) {
   const secondaryTypeColor = getTypeColor(props.secondaryType);
 
   const colorScheme = new Map<string, CardScheme>([
-    [TypeColor.fire, { border: "border-red-400", text: "text-red-400" }],
-    [TypeColor.water, { border: "border-blue-400", text: "text-blue-400" }],
-    [TypeColor.bug, { border: "border-yellow-400", text: "text-yellow-400" }],
-    [TypeColor.grass, { border: "border-green-400", text: "text-green-400" }],
+    [
+      TypeColor.fire,
+      { border: "border-red-400", text: "text-red-400", shadow: "red" },
+    ],
+    [
+      TypeColor.water,
+      { border: "border-blue-400", text: "text-blue-400", shadow: "blue" },
+    ],
+    [
+      TypeColor.bug,
+      { border: "border-yellow-400", text: "text-yellow-400", shadow: "yellow" },
+    ],
+    [
+      TypeColor.grass,
+      { border: "border-green-400", text: "text-green-400", shadow: "green" },
+    ],
     [
       TypeColor.poison,
-      { border: "border-purple-400", text: "text-purple-400" },
+      { border: "border-purple-400", text: "text-purple-400", shadow: "purple" },
     ],
-    [TypeColor.normal, { border: "border-gray-400", text: "text-gray-400" }],
-    [TypeColor.fairy, { border: "border-pink-400", text: "text-pink-400" }],
-    [TypeColor.flying, { border: "border-sky-400", text: "text-sky-400" }],
-    [TypeColor.none, { border: "border-white", text: "text-white" }],
-    [TypeColor.dark, { border: "border-black", text: "text-black" }],
+    [
+      TypeColor.normal,
+      { border: "border-gray-400", text: "text-gray-400", shadow: "gray" },
+    ],
+    [
+      TypeColor.fairy,
+      { border: "border-pink-400", text: "text-pink-400", shadow: "pink" },
+    ],
+    [
+      TypeColor.flying,
+      { border: "border-sky-400", text: "text-sky-400", shadow: "sky" },
+    ],
+    [
+      TypeColor.none,
+      { border: "border-white", text: "text-white", shadow: "white" },
+    ],
+    [
+      TypeColor.dark,
+      { border: "border-black", text: "text-black", shadow: "black" },
+    ],
   ]);
 
   return (
     <div
       className={twMerge(
-        "border-4 rounded-xl min-w-[15rem] w-[23.5rem] h-[28rem] bg-pattern text-white hover:scale-110 transition-all relative shadow-pattern",
+        "border-4 rounded-xl min-w-[15rem] w-[23.5rem] h-[28rem] bg-pattern text-white hover:scale-110 transition-all relative",
         colorScheme.get(mainTypeColor)?.border
       )}
+      style={{
+        boxShadow: `-1px -55px 55px -10px ${colorScheme.get(mainTypeColor)?.shadow} inset`,
+        WebkitBoxShadow: `-1px -55px 55px -10px ${colorScheme.get(mainTypeColor)?.shadow} inset`,
+        MozBoxShadow: `-1px -55px 55px -10px ${colorScheme.get(mainTypeColor)?.shadow} inset`,
+      }}
     >
       <div className="p-4 w-full flex flex-col items-center justify-center h-full">
         <Image src={props.image} alt={props.name} width={250} height={250} />
@@ -49,14 +82,16 @@ function PokemonCard(props: PokemonCardProps) {
         >
           {props.mainType}
         </p>
-        <p
-          className={twMerge(
-            "text-2xl",
-            colorScheme.get(secondaryTypeColor)?.text
-          )}
-        >
-          {props.secondaryType}
-        </p>
+        {props.secondaryType !== "none" && (
+          <p
+            className={twMerge(
+              "text-2xl",
+              colorScheme.get(secondaryTypeColor)?.text
+            )}
+          >
+            {props.secondaryType}
+          </p>
+        )}
       </div>
     </div>
   );
