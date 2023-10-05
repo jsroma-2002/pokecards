56ab6e31ddfbb22f418f96236c23917c9a8c5791
Jose Roncal <joseluis.rome@protonmail.com>
2023-10-04 22:37:08 -0500

    build: :tada: initial commit
    add base layout and page with some pokemon cards

---
+++ components/PokemonCard.tsx
@@ -0,0 +1,9 @@
+function PokemonCard() {
+  return (
+    <div className="border-4 min-w-[23.5rem] max-w-[30%] h-[28rem] p-4 rounded-xl border-neutral-700 bg-neutral-900 text-white shadow-inner shadow-neutral-950">
+        Pokemon Card
+    </div>
+  )
+}
+
+export default PokemonCard
