a28d2e82976fa339a0e7f03b62278621253bb66d
Jose Roncal <joseluis.rome@protonmail.com>
2023-10-04 23:46:29 -0500

    style: :construction: add bg for pokemon card
    add background clas in css to pokemon card

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AppBar() {
   return (
-    <nav className="shadow-inner max-w-[50%] min-w-[25rem] h-full text-black bg-white rounded-3xl flex justify-around items-center hover:scale-110 transition-all shadow-gray-700  border-2 border-white">
+    <nav className="shadow-inner max-w-[50%] min-w-[22rem] h-full text-black bg-white rounded-3xl flex justify-around items-center hover:scale-110 transition-all shadow-gray-700  border-2 border-white">
       <h1 className=" font-bold text-2xl">🃏 Pokemon Card App</h1>
     </nav>
   );
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -1,9 +1,9 @@
 function PokemonCard() {
   return (
-    <div className="border-4 min-w-[23.5rem] max-w-[30%] h-[28rem] p-4 rounded-xl border-neutral-700 bg-neutral-900 text-white shadow-inner shadow-neutral-950">
-        Pokemon Card
+    <div className="border-4 rounded-xl border-neutral-700 min-w-[15rem] w-[23.5rem] h-[28rem]   bg-neutral-900 text-white shadow-inner shadow-neutral-950 hover:scale-110 transition-all relative">
+      <div className="bg-pattern mix-blend-overlay rounded-xl bg-repeat w-full h-full text-white absolute" />
     </div>
-  )
+  );
 }
 
-export default PokemonCard
+export default PokemonCard;
